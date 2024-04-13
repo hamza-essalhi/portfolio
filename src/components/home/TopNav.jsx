e1323e285f79cf3d08a1ab91f0af49dbebd351e0
hamza-essalhi <hamzaessalhix@gmail.com>
2024-04-13 02:22:49 +0000

    v3

--- src/components/home/TopNav.jsx
+++ src/components/home/TopNav.jsx
@@ -1,23 +1,85 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaDownload } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
-
+import { motion } from 'framer-motion';
 
 export default function TopNav() {
+  const [navBackground, setNavBackground] = useState(false);
+  const [animationKey, setAnimationKey] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const windowHeight = window.innerHeight;
+      const threshold = windowHeight * 0.2; // 20% of the window height
+
+      if (scrollPosition > threshold) {
+        setNavBackground(true);
+      } else {
+        setNavBackground(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  // Function to update the animation key whenever navBackground changes
+  useEffect(() => {
+    setAnimationKey(prevKey => prevKey + 1);
+  }, [navBackground]);
 
   return (
-    <nav className="flex gap-3 justify-center items-center p-5 w-full">
-      <div className="flex justify-between items-center w-4/5 max-sm:w-full">
-        <NavLink exact="true" to="/"  >Home</NavLink>
-        <NavLink to="/projects"  >Projects</NavLink>
-        <NavLink to="/about-me"  >About Me</NavLink>
-        <NavLink to="/contact-me"  >Contact Me</NavLink>
-        <div className='w-2/6 flex justify-end'>
-        <button className=' rounded-full  p-3'><span className='max-sm:hidden'>Download Cv</span> <FaDownload className='hidden max-sm:block' /></button>
-
-      </div>
-      </div>
-      
-    </nav>
+    <>
+      {navBackground ? (
+        <motion.nav
+          key={animationKey}
+          className="flex gap-3 justify-center items-center p-5 w-full bg"
+          initial={{ y: navBackground ? -100:0 }}
+          animate={{ y: navBackground ? 0:-100 }}
+          transition={{ duration: 0.5 }}
+        >
+          <div className="flex justify-between items-center w-4/5 max-sm:w-full">
+            <NavLink  to="/">
+              Home
+            </NavLink>
+            <NavLink to="/projects">Projects</NavLink>
+            <NavLink to="/about-me">About Me</NavLink>
+            <NavLink to="/contact-me">Contact Me</NavLink>
+            <div className="w-2/6 flex justify-end">
+              <button className="rounded-full p-3">
+                <span className="max-sm:hidden">Download Cv</span>{' '}
+                <FaDownload className="hidden max-sm:block" />
+              </button>
+            </div>
+          </div>
+        </motion.nav>
+      ) : (
+        <motion.nav
+          key={animationKey}
+          className="flex gap-3 justify-center items-center p-5 w-full"
+          initial={{ y: !navBackground ? -100:0 }}
+          animate={{ y: !navBackground ? 0:-100 }}
+          transition={{ duration: 0.5 }}
+        >
+          <div className="flex justify-between items-center w-4/5 max-sm:w-full">
+            <NavLink  to="/">
+              Home
+            </NavLink>
+            <NavLink to="/projects">Projects</NavLink>
+            <NavLink to="/about-me">About Me</NavLink>
+            <NavLink to="/contact-me">Contact Me</NavLink>
+            <div className="w-2/6 flex justify-end">
+              <button className="rounded-full p-3">
+                <span className="max-sm:hidden">Download Cv</span>{' '}
+                <FaDownload className="hidden max-sm:block" />
+              </button>
+            </div>
+          </div>
+        </motion.nav>
+      )}
+    </>
   );
 }
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { FaFacebookF, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
-
 import { motion } from "framer-motion"
 import { FaXTwitter } from 'react-icons/fa6';
-
+import certifications from '../data/certification.json'
 export default function Home() {
   const [spinDegree, setSpinDegree] = useState(0);
-
   useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
@@ -41,7 +39,7 @@ export default function Home() {
           <motion.h1
             key={i}
             className='max-sm:!text-lg animated-letter mx-1'
-            initial={{ x: -200 - i * Math.floor(Math.random() * (20 - 0)) }}
+            initial={{ x: "-20%" - i * Math.floor(Math.random() * (20 - 0)) }}
             animate={{ x: 0, opacity: 1 }}
             whileHover={{ scale: 1.4 }}
             whileTap={{ scale: 0.8 }}
@@ -56,8 +54,30 @@ export default function Home() {
     }
     return letters;
   };
+
+  const handleHover = (event) => {
+    
+    // Animate the image to the center
+    event.target.style.position = 'fixed';
+    event.target.style.top = `5%`;
+    event.target.style.left = `10%`;
+    event.target.style.zIndex = 9999;
+    event.target.style.width = `90%`;
+    event.target.style.transform = `translate(-50%, -50%) scale(1.1)`;
+
+  };
+
+  const handleHoverEnd = (event) => {
+    // Reset the image position when hover ends
+    event.target.style.position = 'static';
+    event.target.style.top = 'auto';
+    event.target.style.left = 'auto';
+    event.target.style.zIndex = 'auto';
+    event.target.style.width = `100%`;
+  };
+
   return (
-    <div className='home mx-3'>
+    <div className='home mx-10 max-sm:mx-3'>
       <div className="top flex flex-col items-center justify-center gap-5 mx-3">
         <motion.div
           initial={{ opacity: 0 }}
@@ -104,7 +124,6 @@ export default function Home() {
             animate={{ y: 0 }}
             exit={{ y: -700 }}
             transition={{
-
               duration: 1, // Change this to control the overall duration of the animation
               ease: "easeInOut", // You can experiment with different easing functions
             }}
@@ -179,23 +198,99 @@ export default function Home() {
           <div className='flex justify-center'>
             {renderLetters()}
           </div>
-          <div className='flex gap-2 mt-20'>
-            <h3 className='flex-1'>
-              Experienced Freelance Full-Stack Web Developer Specializing in the MERN Stack
-            </h3>
-            <div className='flex flex-col items-center gap-4 map flex-1'>
-              <div className='w-full flex justify-end'>
-              <img className=' max-sm:!w-20' src="/img/mongodb.png" alt="/img/mongodb.png" />
-              </div>
-              <img className=' max-sm:!w-20' src="/img/express.png" alt="/img/express.png" />
-              <div className='w-full flex justify-end'>
-              <img className=' max-sm:!w-20' src="/img/physics.png" alt="/img/physics.png" />
-              </div>
-             
-              <img className=' max-sm:!w-20' src="/img/node-js.png" alt="/img/node-js.png" />
+          <div className='flex gap-6 mt-20 items-center max-md:flex-col'>
+            <motion.h3
+              initial={{ x: "-20%", scale: 0 }}
+              whileInView={{ x: 0, scale: 1 }}
+              transition={{ duration: 0.4 }}
+              viewport={{ once: true }}
+              className='flex-1'>
+              Experienced freelance full-stack web developer specializing in the MERN Stack. A MERN developer specializes in creating web applications using MongoDB, Express.js, React.js, and Node.js. They build dynamic and interactive websites, handling both front-end and back-end development tasks. MERN developers are skilled in JavaScript and utilize these technologies to design efficient and scalable applications.
+            </motion.h3>
+            <div className='flex items-center gap-4 map flex-1 '>
+
+              <motion.img
+                initial={{ x: "-20%", scale: 0 }}
+                whileInView={{ x: 0, scale: 1 }}
+                transition={{ duration: 0.4 }}
+                viewport={{ once: true }}
+                className='max-sm:!w-16 max-sm:!h-16' src="/img/mongodb.png" alt="/img/mongodb.png" />
+
+              <motion.img
+                initial={{ x: "-20%", scale: 0 }}
+                whileInView={{ x: 0, scale: 1 }}
+                transition={{ duration: 0.4 }}
+                viewport={{ once: true }}
+                className=' max-sm:!w-16 max-sm:!h-16' src="/img/express.png" alt="/img/express.png" />
+
+              <motion.img
+                initial={{ x: "-20%", scale: 0 }}
+                whileInView={{ x: 0, scale: 1 }}
+                transition={{ duration: 0.4 }}
+                viewport={{ once: true }}
+                className=' max-sm:!w-16 max-sm:!h-16' src="/img/physics.png" alt="/img/physics.png" />
+
+              <motion.img
+                initial={{ x: "-20%", scale: 0 }}
+                whileInView={{ x: 0, scale: 1 }}
+                transition={{ duration: 0.4 }}
+                viewport={{ once: true }}
+                className=' max-sm:!w-16 max-sm:!h-16' src="/img/programing.png" alt="/img/programing.png" />
             </div>
           </div>
         </div>
+      </div>
+      <div className="flex gap-4 mx-3 mt-16 flex-col items-center about">
+        <div className="flex flex-col flex-1 items-center">
+          <motion.h1
+          initial={{ x: "-20%", scale: 0 }}
+          whileInView={{ x: 0, scale: 1 }}
+          transition={{ duration: 0.4 }}
+          viewport={{ once: true }}
+            className='my-20 max-sm:!text-lg'>About Me</motion.h1>
+          <motion.p 
+          initial={{ x: "-20%", scale: 0 }}
+          whileInView={{ x: 0, scale: 1 }}
+          transition={{ duration: 0.4 }}
+          viewport={{ once: true }}
+          className=''>Hello! I'm Hamza, a 24-year-old full-stack web developer based in Casablanca. I hold a university degree in Analytical Chemistry and Environment, along with a Bachelor's Degree in Physical Science and Chemistry. I specialize in the MERN stack, crafting dynamic and interactive web applications. I'm passionate about learning and have completed various certifications in web development, including front-end, full-stack JavaScript, and Python programming. My goal is to create efficient, scalable, and user-friendly applications that leave a positive impact.</motion.p>
+          <motion.h1
+          initial={{ x: "-20%", scale: 0 }}
+          whileInView={{ x: 0, scale: 1 }}
+          transition={{ duration: 0.4 }}
+          viewport={{ once: true }} className='my-20 max-sm:!text-lg'>Certifications</motion.h1>
+
+          <div className="certifications flex flex-col gap-5">
+            {
+              certifications.map((certification, i) => (
+                <motion.div
+              
+          initial={{ x: "-20%", scale: 0 }}
+          whileInView={{ x: 0, scale: 1 }}
+          transition={{ duration: 0.4 }}
+          viewport={{ once: true }}
+                 key={i} className={`flex gap-10 w-full max-md:!justify-normal ${i % 2 === 0 ? "justify-end" : "justify-normal"}`}>
+                  <div className="certification p-3 w-2/4 flex flex-col gap-5 max-lg:w-2/3 max-sm:!w-full">
+                    <a href={certification.link} className="my-5">{certification.title}</a>
+                    <div className="image-container relative">
+                      <motion.img
+
+                        src={certification.img}
+                        alt={certification.title}
+                        className="hover-img"
+                        whileHover={{ scale:1.2}}
+                       
+                      />
+
+                    </div>
+                  </div>
+                </motion.div>
+              ))
+            }
+          </div>
+
+        </div>
+
       </div>
       <div className="middel flex gap-4 mx-3 mt-16">
         <div className="col flex-1">1</div>
